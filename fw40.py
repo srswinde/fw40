d6b25fd2adbb3174be8821bdb2f409668a99b28c
scott <scottswindell@email.arizona.edu>
2016-10-17 12:25:46 -0700

    A few improvements to fw40.py

--- fw40.py
+++ fw40.py
@@ -2,7 +2,12 @@ import socket
 import time
 import sys
 
+"""Simple python interface to the 40" fitler wheel."""
+
 def conv( msg="CSS40 FW40 123 REQUEST FNUM\n", ip="192.168.2.46", port=5750 ):
+	"""
+		Handle the back meat of the socket connection
+	"""
 	soc = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 	
 	
@@ -22,16 +27,65 @@ def conv( msg="CSS40 FW40 123 REQUEST FNUM\n", ip="192.168.2.46", port=5750 ):
 
 
 class fw:
+	"""Class to create easy interface to the css40 filter wheel. It uses ng protocol
+	and a short set of requests and commands"""
+	def __init__(self, obsid="CSS40", sysid="FW40", ip='192.168.2.46', port=5750 ):
+		self.obsid = obsid
+		self.sysid = sysid
+		self.ip = ip
+		self.port = port
+		self.refnum = 0
 
-	def getall(self):
-		vals = conv().split()[3:]
-		return {'home':int( vals[0] ), 'com':float( vals[1] ), 'cur':float( vals[2]) }
-	def go(self):
-		return conv("CSS40 FW40 123 COMMAND MOVE\n")
+	def command( self, cmdname , arglist=[]):
+		"""Send a command to the filter wheel"""
+		if type(arglist) in (str, int, float):
+			arglist = [arglist]
+		elif type(arglist) == list:
+			pass
+		else:
+			raise TypeError( 'Arglist must be a string int or float or a list of these types.' )
+		
+		argstr = ""
+		for arg in arglist:
+			argstr = argstr + str( arg ) +' '
+		cmdstr = "{} {} {} COMMAND {} {}\n".format( self.obsid, self.sysid, self.refnum, cmdname, argstr)
+		self.refnum = (self.refnum+1) % 999
+		return conv( cmdstr, self.ip, self.port  )
 
-	def stop( self ):
-		return conv("CSS40 FW40 123 COMMAND STOP\n")
+	def request(self, reqname, arglist=[] ):
+		"""Send a request to the filter wheel"""
+		if type(arglist) in (str, int, float):
+			arglist = [arglist]
+		elif type(arglist) == list:
+			pass
+		else:
+			raise TypeError( 'Arglist must be a string int or float or a list of these types.' )
+		
+		argstr = ""
 
-	def goto( self, num ):
-		return conv("CSS40 FW40 123 COMMAND GOTO {0}\n".format(num) )
+		for arg in arglist:
+			argstr = argstr + str( arg ) +' '
+		cmdstr = "{} {} {} REQUEST {} {}\n".format( self.obsid, self.sysid, self.refnum, reqname, argstr)
+		self.refnum = (self.refnum+1) % 999
+		return conv( cmdstr, self.ip, self.port  )
+		
+	def reqMOVE(self):
+		"""Move the filter wheel 1 filter over"""
+		return self.command("MOVE")
+
+	def comSTOP( self ):
+		"""Stop all motion"""
+		return self.command("STOP")
+
+	def comGOTO( self, num ):
+		"""Go to the specified filter by number"""
+		return self.command( "GOTO", num )
+	
+	def reqMOT( self ):
+		"""Returns the motion status"""
+		return self.request("MOT")
+
+	def reqFNUM(self):
+		"""Returns the current filter number"""
+		return self.request('FNUM')
 	
